87a5497827ee63fb51d58528c5d4fc3afa2bbe15
unknown <Trinet@WIN10.trinet.si>
2015-02-09 13:54:05 +0100

    Revert "no message"
    This reverts commit 083fa605ff30e457b190d9378ffad75283128e01.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "string"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,15 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_loginButton_clicked()
+{
+    string user, pass;
+    ui->loginButton->hide();
+    ui->UserLabel->hide();
+    ui->PassLabel->hide();
+    ui->UserText->hide();
+    ui->PassText->hide();
+    ui->label->setText("Dobrodošli!");
 }
--- untitled.pro
+++ untitled.pro
@@ -1,6 +1,6 @@
 #-------------------------------------------------
 #
-# Project created by QtCreator 2015-02-09T13:38:22
+# Project created by QtCreator 2015-02-09T12:23:17
 #
 #-------------------------------------------------
 
@@ -8,7 +8,7 @@ QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-TARGET = Login_Project
+TARGET = untitled
 TEMPLATE = app
 
 
