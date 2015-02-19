4c4f17f846d8ad76dea35ecac16e065b0a39fb90
Miha Smrajec <msmrajec@yahoo.com>
2015-02-19 21:23:41 +0100

    Popravljene napake prejsnega commita

---
+++ mainwindow.cpp
@@ -0,0 +1,146 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QString"
+#include "QMessageBox"
+#include "QTextStream"
+#include "QFile"
+#include "QDebug"
+#include "QDateTime"
+#include "QTimer"
+#include "algorithm"
+#include "QCryptographicHash"
+#include "QByteArray"
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QTimer *timer=new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(show_time()));
+    timer->start();
+}
+
+void MainWindow::show_time()
+{
+    QTime time=QTime::currentTime();
+    QString time_text=time.toString("hh : mm : ss");
+    ui->label->setText(time_text);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+int loggedin=0;
+
+QString temp, user, pass;
+
+int checkLogin(QString username, QString password)
+{
+    if (username != "" && password != "") {
+    QFile dat("C:/Users/XXXXX/Documents/LoginProject/skupno.txt");
+    dat.open(QIODevice::ReadOnly | QFile::Text);
+    QTextStream in(&dat);
+      while (!in.atEnd()) {
+         temp = in.readLine();
+         user = temp.section("#",0,0);
+         pass = temp.section("#",1,1);
+
+         if (username == user) {
+             if (QString(QCryptographicHash::hash((password.toUtf8()),QCryptographicHash::Md5).toHex()) == pass) {
+                 return 1;
+             }
+         }
+      }
+    }
+    return 0;
+}
+
+void MainWindow::on_LogIn_clicked()
+{
+    QMessageBox msgBox;
+    if(loggedin==0)
+        {
+        loggedin = checkLogin(ui->UserEdit->text(),ui->PassEdit->text());
+        if(loggedin==1)
+            {
+            ui->UserLabel->hide();
+            ui->UserEdit->hide();
+            ui->PassLabel->hide();
+            ui->PassEdit->hide();
+            ui->LogIn->setText("ReLog");
+            ui->Label->setText("Dobrodošli");
+            ui->UserEdit->clear();
+            ui->PassEdit->clear();
+        }
+        else
+            {
+            msgBox.warning(this, tr("Warning!"), tr("Napačno uporabniško ime ali geslo!"));
+        }
+    }
+    else
+    {
+        ui->UserLabel->show();
+        ui->UserEdit->show();
+        ui->PassLabel->show();
+        ui->PassEdit->show();
+        ui->LogIn->setText("LogIn");
+        ui->Label->setText("");
+        loggedin=0;
+    }
+}
+
+void MainWindow::on_PassEdit_returnPressed()
+{
+    ui->LogIn->clicked();
+}
+
+void MainWindow::on_UserEdit_returnPressed()
+{
+    ui->PassEdit->setFocus();
+    if(ui->UserEdit->text()=="")
+        ui->PassEdit->clear();
+}
+void MainWindow::on_pushButton_clicked()
+{
+    QString s;
+    QMessageBox msgBox;
+        switch (loggedin) {
+        case 1:
+            s="Uporabnik je prijavljen";
+            break;
+        default:s="Uporabnik ni prijavljen";
+            break;
+        }
+        msgBox.warning(this, tr("Opomba"), s);
+    }
+void MainWindow::on_Register_pressed()
+{
+    if(ui->UserEdit->text() != ui->PassEdit->text() && ui->UserEdit->text()!="")
+        {
+        QString strImena, strGesla;
+        QFile dat("C:/Users/XXXXX/Documents/LoginProject/skupno.txt");
+        strImena=ui->UserEdit->text();
+        strGesla=ui->PassEdit->text();
+
+       try {
+            dat.open(QIODevice::Append | QFile::Text);
+            QTextStream out(&dat);
+            out << strImena + "#";
+            out << QString(QCryptographicHash::hash((strGesla.toUtf8()),QCryptographicHash::Md5).toHex()) + "\n";
+        }
+        catch(const exception& e) {
+            QMessageBox::critical(0,"Error",e.what());
+        };
+        dat.flush();
+        dat.close();
+    }
+    else if(ui->UserEdit->text()=="")
+        QMessageBox::information(0,"Error","Za registriranje vpiši Uporabniško ime!");
+    else if(ui->UserEdit->text() == ui->PassEdit->text())
+        QMessageBox::information(0,"Error","Za registriranje uporabi različno Uporabniško ime, kot Geslo!");
+    }
